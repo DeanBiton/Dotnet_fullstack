339d95701e59d586aae377921323c7c062f50ec0
unknown <dinbiton100@gmail.com>
2023-04-09 14:49:05 +0300

    get data from randomAPI

--- RestApi-CS/Services/CustomerService.cs
+++ RestApi-CS/Services/CustomerService.cs
@@ -10,14 +10,18 @@ namespace RestAPI.Services;
 public class CustomerService
 {
     SqlConnection con;
-    
+    private readonly HttpClient client;
+
     public CustomerService()
     {
+        client = new HttpClient();
         con = new SqlConnection("server=DESKTOP-5R5EJ4F; database=Final; Integrated Security=True;");
     }
 
     async public Task<DataTable> Get()
     {
+        JsonNode jsonNode = await useRandomUserApi("http://randomuser.me/api/?results=2&gender=male");
+        Console.WriteLine(jsonNode.ToString());
         SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Customer", con);
         DataTable dt = new DataTable();
         da.Fill(dt);
@@ -51,5 +55,18 @@ public class CustomerService
         int i = cmd.ExecuteNonQuery();
         con.Close();
         return i;
+    }
+
+    async private Task<JsonNode> useRandomUserApi(string url)
+    {
+        JsonNode? jsonNode = null;
+        var response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        var jsonObject = await response.Content.ReadFromJsonAsync<JsonObject>();
+        jsonObject?.TryGetPropertyValue("results", out jsonNode);
+        if (jsonNode == null)
+            throw new Exception(String.Format("Failed to get results from url: {0}", url));
+
+        return jsonNode;
     }
 }
